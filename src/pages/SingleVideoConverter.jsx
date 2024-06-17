9ffc2513755ff7b74ec157df6393ed7c8c39e055
Temidayo32 <brainiedude@gmail.com>
2024-06-17 12:32:14 +0100

    page: VideoConverter page
    added main landing page for VideoConverters

--- src/pages/SingleVideoConverter.jsx
+++ src/pages/SingleVideoConverter.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { useParams, useLocation } from 'react-router-dom';
-import { UploadVideo, HowToConvert } from '../components';
+import { UploadVideo, HowToConvert, FaqSection } from '../components';
 
 const SingleVideoConverter = () => {
   const { format } = useParams();
@@ -15,10 +15,11 @@ const SingleVideoConverter = () => {
   
   return (
     <div className="container mx-auto py-20"  key={key}>
-      <h1 className="text-5xl py-4 text-center font-bold text-teal-800">{format.toUpperCase()} Converter</h1>
+      <h1 className="text-3xl md:text-4xl lg:text-7xl py-4 text-center font-bold text-teal-800">{format.toUpperCase()} Converter</h1>
       <p className="text-gray-700 text-lg text-center">Convert your videos to {format.toUpperCase()} format, for free.</p>
       <UploadVideo defaultFormat={format} />
       <HowToConvert />
+      <FaqSection/>
     </div>
     
   );
--- src/pages/VideoConverter.jsx
+++ src/pages/VideoConverter.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import { VideoHero, VideoSection, HowToConvert } from '../components'
+import React, {useState, useEffect} from 'react'
+import { useParams, useLocation } from 'react-router-dom';
+import { VideoHero, VideoSection, HowToConvert, FaqSection } from '../components'
 
 function VideoConverter() {
+  const [key, setKey] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); 
+    setKey((prevKey) => prevKey + 1);
+  }, [ location.pathname]);
+
+
   return (
-    <div>
+    <div key={key}>
         <VideoHero/>
-        <VideoSection numConversionsToShow={6} numConversions={16} hideButton={false} />
+        <VideoSection numConversionsToShow={6} numConversions={16} hideButton={false} moreLess={true} />
         <HowToConvert/>
+        <FaqSection/>
       
     </div>
   )
